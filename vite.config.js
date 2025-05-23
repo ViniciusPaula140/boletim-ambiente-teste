e44df8cd117a928c024201cf7182c2953a5cd356
Vinícius Paula <164179459+ViniciusPaula140@users.noreply.github.com>
2025-05-23 17:24:19 -0300

    A

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,10 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'client/src'),
-      '@shared': path.resolve(__dirname, 'shared'),
-      '@assets': path.resolve(__dirname, 'attached_assets'),
-    },
-  },
-  root: path.resolve(__dirname, 'client'),
+  root: 'client',
   build: {
-    outDir: path.resolve(__dirname, 'dist/public'),
-    emptyOutDir: true,
-    sourcemap: true
+    outDir: '../dist/public',
+    emptyOutDir: true
   },
   server: {
     port: 3000,
