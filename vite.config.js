a2d416bebc0d561dc1391bacd4091004f2904543
Vinícius Paula <164179459+ViniciusPaula140@users.noreply.github.com>
2025-05-23 17:30:24 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,12 +9,6 @@ export default defineConfig({
     emptyOutDir: true
   },
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
+    port: 3000
   }
 }); 
